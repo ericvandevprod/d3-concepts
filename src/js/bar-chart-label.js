6bb9e45a72f94e747c2e3a57cc97136a8f4ac37b
Eric Vandenberg <eric@vandevprod.com>
2017-03-13 23:49:03 -0700

    Random gen data on click

--- src/js/bar-chart-label.js
+++ src/js/bar-chart-label.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 
-const w = 500;
-const h = 100;
+const w = 600;
+const h = 250;
 const pad = 1;
 
 let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
@@ -43,17 +43,24 @@ body.appendChild(update);
 
 d3.select("p")
     .on("click", function() {
-      //New values for dataset
-      dataset = [ 11, 12, 15, 20, 18, 17, 16, 18, 23, 25,
-        5, 10, 13, 19, 21, 25, 22, 18, 15, 13 ];
-      //Update all rects
+      let numValues = dataset.length;
+      dataset = [];
+      for (let i = 0; i < numValues; i++) {
+        let newNumber = Math.floor(Math.random() * 35 + 5);
+        dataset.push(newNumber);
+      }
       svg.selectAll("rect")
           .data(dataset)
+          .transition()
+          .delay((d, i) => i / (dataset.length * 1000))
+          .duration(500)
           .attr("y", (d) => h - (d * 4))
           .attr("height", (d) => d * 4);
 
       svg.selectAll('text')
           .data(dataset)
+          .transition()
+          .duration(1000)
           .text((d) => d)
           .attr('x', (d, i) => i * (w / dataset.length) + (w / dataset.length - pad) / 2)
           .attr('y', (d) => h - (d * 4) + 14)
